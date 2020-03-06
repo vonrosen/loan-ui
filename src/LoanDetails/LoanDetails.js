c5f331a32861c05c37fc51fbaf2bed3dc43dc83b
Hunter Stern <hunter.stern@autodesk.com>
2020-03-06 15:38:00 -0800

    more changes

--- src/LoanDetails/LoanDetails.js
+++ src/LoanDetails/LoanDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyledDiv } from './LoanDetailsStyle';
 import LoanDetailsBarChartLabel from './LoanDetailsBarChartLabel';
-import { Label, BarChart, Bar, XAxis, YAxis } from 'recharts';
+import { ResponsiveContainer, Label, BarChart, Bar, XAxis, YAxis } from 'recharts';
 import { formatLoanDataForChart } from '../utils';
 
 class LoanDetails extends React.Component {
@@ -21,15 +21,18 @@ class LoanDetails extends React.Component {
                 <h1>
                     {term} year fixed
                 </h1>
-                <BarChart margin={{ top: 0, right: 30, bottom: 0, left: 30 }} width={1800} height={350} data={formattedData}>
-                    <XAxis dataKey="Rate" tick={{ fontSize: 20 }} />
-                    <YAxis padding={{ top: 40 }} dataKey="LoanAmount" tick={{ fontSize: 14 }} type="number" domain={[0, maxLoanValue]} tickFormatter={
-                        (v) => ( v.toLocaleString('en-US', { style: 'currency', currency: 'USD' }).split(".")[0] )}>
-                        <Label value="Loan Amount" position="insideTopLeft" fill="white" fontSize={24} />
-                    </YAxis>
-                    <Bar isAnimationActive={false} dataKey="LoanAmount" fill="white" barSize={30} barGap={200}
-                        label={<LoanDetailsBarChartLabel fontSize="14" fill="white" position="top" />}/>
-                </BarChart>
+                <ResponsiveContainer minHeight={350} minWidth={800}>
+                    <BarChart margin={{ top: 0, right: 30, bottom: 0, left: 30 }} data={formattedData}>
+                        <XAxis dataKey="Rate" tick={{ fontSize: 20 }} />
+                        <YAxis padding={{ top: 40 }} dataKey="LoanAmount" tick={{ fontSize: 12 }} type="number"
+                            domain={[0, maxLoanValue]} tickFormatter={
+                                (v) => ( v.toLocaleString('en-US', { style: 'currency', currency: 'USD' }).split(".")[0] )}>
+                            <Label value="Loan Amount" position="insideTopLeft" fill="white" fontSize={24} />
+                        </YAxis>
+                        <Bar isAnimationActive={false} dataKey="LoanAmount" fill="white" barSize={30} barGap={200}
+                            label={<LoanDetailsBarChartLabel fontSize="14" fill="white" position="top" />}/>
+                    </BarChart>
+                </ResponsiveContainer>
             </StyledDiv>
         );
     }
